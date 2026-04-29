8de6fa18190c65d76ae99e17476453e7f2c295bc
fatsheepER <fatsheeper@icloud.com>
2026-04-29 17:50:35 +0800

    syntax: create source files based on designed structure

--- syntax_analysis_top_down/src/main.cpp
+++ syntax_analysis_top_down/src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 
-int main(int, char**){
-    std::cout << "Hello, from top_down_syntax_analysis!\n";
+int main(int, char **) {
+	std::cout << "Hello, from top_down_syntax_analysis!\n";
 }
